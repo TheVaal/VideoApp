e850cc8c971f1293addf6049e0a402309f180d1e
TheVaal <kryvoruchko142@gmail.com>
2024-09-24 11:59:24 +0300

    created project

--- app/src/main/java/com/vaalzebub/next/to/videoapp/MainActivity.kt
+++ app/src/main/java/com/vaalzebub/next/to/videoapp/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.vaalzebub.next.to.videoapp.presentation.NavGraphs
--- app/src/main/java/com/vaalzebub/next/to/videoapp/ui/theme/Theme.kt
+++ app/src/main/java/com/vaalzebub/next/to/videoapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.vaalzebub.next.to.videoapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
